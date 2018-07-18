a2f6d5b22aaeb7dd50be10d2828ff13be7f01df0
康帅傅六个柠檬C <tang14455@163.com>
2018-07-18 16:38:15 +0800

    Initial commit

---
+++ Senparc.Weixin.MP/Senparc.Weixin.MP/AdvancedAPIs/TemplateMessage/TemplateMessageJson/TempleteModel.cs
@@ -0,0 +1,66 @@
+﻿#region Apache License Version 2.0
+/*----------------------------------------------------------------
+
+Copyright 2017 Jeffrey Su & Suzhou Senparc Network Technology Co.,Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the
+License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the specific language governing permissions
+and limitations under the License.
+
+Detail: https://github.com/JeffreySu/WeiXinMPSDK/blob/master/license.md
+
+----------------------------------------------------------------*/
+#endregion Apache License Version 2.0
+
+/*----------------------------------------------------------------
+    Copyright (C) 2017 Senparc
+    
+    文件名：TempleteModel.cs
+    文件功能描述：模板消息接口需要的数据
+    
+    
+    创建标识：Senparc - 20150211
+    
+    修改标识：Senparc - 20150303
+    修改描述：整理接口
+----------------------------------------------------------------*/
+
+namespace Senparc.Weixin.MP.AdvancedAPIs.TemplateMessage
+{
+    public class TempleteModel
+    {
+        /// <summary>
+        /// 目标用户OpenId
+        /// </summary>
+        public string touser { get; set; }
+
+        /// <summary>
+        /// 模板ID
+        /// </summary>
+        public string template_id { get; set; }
+
+        /// <summary>
+        /// 模板消息顶部颜色（16进制），默认为#FF0000
+        /// </summary>
+        public string topcolor { get; set; }
+
+        /// <summary>
+        /// 数据
+        /// </summary>
+        public object data { get; set; }
+
+        public string url { get; set; }
+
+
+        public TempleteModel()
+        {
+            topcolor = "#FF0000";
+        }
+    }
+}
---
+++ Senparc.Weixin.MP/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Senparc.Weixin.Helpers;
+using Senparc.Weixin.MP;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP.AdvancedAPIs.Card;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            string accessToken = "11_jFGzse3a4wGPBq8cCTsKUMGabo2V_dmJVKHKiltmkNmhTSKHv8J6BaJ5nTQ6-eTeNhdh1LNNOcGEkbYelx6DlFaSx9wIcOIlaKGMMJujb9BptSNhWnDq7Vpyb_oQuFT_nYJpp7bOnT5ew1kESKBcACAMEV";
+
+            var data1 = new Card_GrouponData()
+            {
+                base_info = _BaseInfo,
+                deal_detail = "测试"
+            };
+
+            var result1 = CardApi.CreateCard(accessToken, data1);
+
+            var data = new Card_MemberCardData()
+            {
+                base_info = _BaseInfo,
+                supply_bonus = true,
+                supply_balance = false,
+                prerogative = "123123",
+                bind_old_card_url = "www.daidu.com",
+                wx_activate = true
+            };
+
+            var result = CardApi.CreateCard(accessToken, data);
+
+           
+            return new string[] { "value1", "value2" };
+        }
+
+        protected Card_BaseInfoBase _BaseInfo = new Card_BaseInfoBase()
+        {
+            logo_url = "http:\\www.supadmin.cn/uploads/allimg/120216/1_120216214725_1.jpg",
+            brand_name = "海底捞",
+            code_type = Card_CodeType.CODE_TYPE_TEXT,
+            title = "132 元双人火锅套餐",
+            sub_title = "周末狂欢必备",
+            color = "Color010",
+            notice = "使用时向服务员出示此券",
+            service_phone = "020-88888888",
+            description = @"不可与其他优惠同享\n 如需团购券发票，请在消费时向商户提出\n 店内均可
+使用，仅限堂食\n 餐前不可打包，餐后未吃完，可打包\n 本团购券不限人数，建议2 人使用，超过建议人
+数须另收酱料费5 元/位\n 本单谢绝自带酒水饮料",
+            date_info = new Card_BaseInfo_DateInfo()
+            {
+                type = Card_DateInfo_Type.DATE_TYPE_PERMANENT.ToString(),
+            },
+            sku = new Card_BaseInfo_Sku()
+            {
+                quantity = 5
+            },
+            use_limit = 1,
+            get_limit = 3,
+            use_custom_code = false,
+            bind_openid = false,
+            can_share = true,
+            can_give_friend = true,
+            url_name_type = Card_UrlNameType.URL_NAME_TYPE_RESERVATION,
+            custom_url = "http://www.weiweihi.com",
+            source = "大众点评",
+            custom_url_name = "立即使用",
+            custom_url_sub_title = "6个汉字tips",
+            promotion_url_name = "更多优惠",
+            promotion_url = "http://www.qq.com",
+        };
+
+
+    }
+}
